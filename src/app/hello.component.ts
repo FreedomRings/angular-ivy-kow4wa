01e96f7c491957e2620d70adf8472a8f61c3b222
FreedomRings <jeff@pointhere.net>
2022-02-21 14:38:56 -0500

    4 files modified, 3 added

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,9 +2,18 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  template: `
+  <h1>Chart.js Plugin Example</h1>
+  <h2>chartjs-plugin-doughnutlabel-v3</h2>
+  <h3>(Example in {{name}})</h3>`,
+  styles: [
+    `
+    h1 { font-family: Lato; text-align: center; }
+    h2 { font-family: Lato; text-align: center; color: blue }
+    h3 { font-family: Lato; text-align: center; color: slategrey; }
+    `,
+  ],
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
 }
