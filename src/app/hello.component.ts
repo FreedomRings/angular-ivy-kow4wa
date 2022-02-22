7177199bbb835e2a4ec7c4268400eea3f665ca27
FreedomRings <jeff@pointhere.net>
2022-02-21 19:34:43 -0500

    Final commit for working angular sample

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -3,14 +3,12 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'hello',
   template: `
-  <h1>Chart.js Plugin Example</h1>
-  <h2>chartjs-plugin-doughnutlabel-v3</h2>
-  <h3>(Example in {{name}})</h3>`,
+  <h1>Chart.js Angular Plugin Example</h1>
+  <h2>(Example in {{name}})</h2>`,
   styles: [
     `
     h1 { font-family: Lato; text-align: center; }
     h2 { font-family: Lato; text-align: center; color: blue }
-    h3 { font-family: Lato; text-align: center; color: slategrey; }
     `,
   ],
 })
